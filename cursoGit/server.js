17b08756bd77bee0b54e04362e62d4b3c4ca50cf
marcelo <fotofacil@live.com>
2019-09-27 10:02:28 -0300

    aplicando nova rota

--- cursoGit/server.js
+++ cursoGit/server.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
     res.send('Rota ok')
 })
 
+app.get('/bugou' ,(req, res)=>{
+    res.send("bugou tudo")
+})
+
 app.listen(3033, ()=>{
     console.log('tudo funcionando')
 })
